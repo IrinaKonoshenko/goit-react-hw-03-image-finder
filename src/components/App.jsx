66e93c76ab7e81497238f4c08891f64940fef606
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2022-12-06 18:53:31 +0600

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,49 +4,41 @@ import { Modal } from './Modal/Modal';
 import { Component } from 'react';
 import { Empty } from './Empty/Empty';
 import { Loader } from './Loader/Loader';
-import axios from 'axios';
 import { Button } from './Button/Button';
+import { Api } from 'helpers/Api';
 
 export class App extends Component {
-  constructor() {
-    super();
+  state = {
+    images: [],
+    page: 1,
+    isLoading: false,
+    query: '',
+    total: 13,
+    current: null,
+  };
 
-    this.state = {
-      images: [],
-      page: 1,
-      isLoading: false,
-      query: '',
-      total: 13,
-      current: null,
-    };
-  }
-
-  fetchImages = (isUpdate = false) => {
+  fetchImages = () => {
     this.setState({
       isLoading: true,
     });
-    axios
-      .get(
-        `https://pixabay.com/api/?q=${this.state.query}&page=${this.state.page}&key=30782828-18aebbc8281f2a6373b4ce5f4&image_type=photo&orientation=horizontal&per_page=12`
-      )
-      .then(res => {
-        this.setState({
-          images: isUpdate
-            ? [...this.state.images, ...res.data.hits]
-            : res.data.hits,
-          total: res.data.total,
-        });
-      })
-      .finally(() => {
-        this.setState({
-          isLoading: false,
-        });
+    Api.then(res => {
+      this.setState({
+        images: [...this.state.images, ...res.data.hits],
+        total: res.data.total,
       });
+    }).finally(() => {
+      this.setState({
+        isLoading: false,
+      });
+    });
   };
 
   componentDidUpdate(_, prevState) {
-    if (prevState.page !== this.state.page) {
-      this.fetchImages(true);
+    if (
+      prevState.page !== this.state.page ||
+      prevState.query !== this.state.query
+    ) {
+      this.fetchImages();
     }
   }
 
@@ -54,17 +46,17 @@ export class App extends Component {
     return this.state.page >= Math.ceil(this.state.total / 12);
   }
 
+  onSubmit = query => {
+    this.setState({ query, page: 1, images: [] });
+  };
+
   render() {
     const hasImages = this.state.images.length > 0;
     const hasLoading = this.state.isLoading && !hasImages;
 
     return (
       <div className="App">
-        <Searchbar
-          onSubmit={query =>
-            this.setState({ query, page: 1 }, () => this.fetchImages(false))
-          }
-        />
+        <Searchbar onSubmit={this.onSubmit} />
         {hasImages ? (
           <ImageGallery
             onClickImage={image => this.setState({ current: image })}
